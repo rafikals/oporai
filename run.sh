7b26b2c17d6e0a7bcf94c7f7e5b1b8b209bed4a4
Rafikal Septiadi <115643114+rafikals@users.noreply.github.com>
2024-03-21 23:06:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-72890fb5-c933-44a7-ab75-744f90725aad/)
 
 
 echo "$curl_output" >> Update.tsx
